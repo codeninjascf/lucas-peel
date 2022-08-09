12e68161924f56b11607bd66084ca16efa6b181f
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2022-08-09 13:00:36 +0100

    thing

--- Dropping bombs pt. 1/Assets/Scripts/Reset.cs
+++ Dropping bombs pt. 1/Assets/Scripts/Reset.cs
@@ -1,12 +1,37 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class Reset : MonoBehaviour
 {
+    public Animator clouds;
+    public GameObject explosion;
+    private Transform _transform;
+    private MeshRenderer _meshRenderer;
+    private Collider _collider;
+
+    private void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _collider = GetComponent<Collider>();
+        _transform = transform;
+    }
+
     // Called when the object collides with something
     private void OnCollisionEnter(Collision collision)
     {
-        // Loads the initial scene, resetting the game
+        collision.gameObject.SetActive(false);
+        StartCoroutine(ResetScene());
+    }
+    
+    private IEnumerator ResetScene()
+    {
+        _meshRenderer.enabled = false;
+        _collider.enabled = false;
+        clouds.enabled = false;
+        transform.GetChild(0).gameObject.SetActive(false);
+        Instantiate(explosion, _transform.position, _transform.rotation);
+        yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene(0);
     }
 }
