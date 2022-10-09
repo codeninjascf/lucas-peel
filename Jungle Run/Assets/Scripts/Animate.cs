543550f9c3e66936079e0a5dd768e355f6e01d59
codeninjascf <cnchandlersford@gmail.com>
2022-10-09 12:37:07 +0100

    update

--- Jungle Run/Assets/Scripts/Animate.cs
+++ Jungle Run/Assets/Scripts/Animate.cs
@@ -16,6 +16,9 @@ public class Animate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(_jump.GetIsGrounded())
+        {
+
+        }
     }
 }
