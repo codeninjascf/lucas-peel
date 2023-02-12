8280e1c2da5354947ea8564a2e0b0d259b248fea
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2023-02-12 12:13:09 +0000

    FIXED GIANT BUG start from page 5 of 8 next time (food frenzy part 2 google docs)
    temporary

--- Food Frenzy/Assets/Scripts/GameManager.cs
+++ Food Frenzy/Assets/Scripts/GameManager.cs
@@ -1,53 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     public Level level;
     public GridManager gridManager;
-    public UIManager uiManager
+    
     private bool _gameEnded;
+
+    public UIManager uiManager;
     // Start is called before the first frame update
     void Start()
     {
         _gameEnded = false;
-        if(level.type==LevelType.Moves)
+
+        if (level.type == LevelType.Moves)
         {
             uiManager.criteriaInfoText.text = "MovesRemaing";
         }
-        else if(level.type==LevelType.Time)
+        else if (level.type == LevelType.Time)
         {
             uiManager.criteriaInfoText.text = "Time Remaining:";
         }
+
         uiManager.highScoreNumber.text = level.HighScore.ToString();
         uiManager.gameOverMenu.SetActive(false);
     }
+
     // Update is called once per frame
     void Update()
     {
-        if(level.type==LevelType.Moves)
+        if (level.type == LevelType.Moves)
         {
             uiManager.criteriaNumber.text = level.MovesRemaining.ToString();
         }
-        else if(level.type==LevelType.Time)
+        else if (level.type == LevelType.Time)
         {
             double timeRemaining = Math.Max(level.TimeRemaining, 0);
             TimeSpan time = TimeSpan.FromSeconds(timeRemaining);
             uiManager.criteriaNumber.text = time.ToString(@"mm\:ss");
         }
+            if (level.GameOver && gridManager.MoveComplete && !_gameEnded)
+            {
+                _gameEnded = true;
+            }
 
-        if (level.GameOver && gridManager.MoveComplete && !_gameEnded)
-        {
-            _gameEnded = true;
-        }
         int starsAchieved = level.StarsAchieved(gridManager.Score);
         uiManager.UpdateScores(gridManager.Score);
         uiManager.UpdateStars(starsAchieved);
+        if (level.GameOver && gridManager.MoveComplete && !_gameEnded)
+        {
+            _gameEnded = true;
+            gridManager.GameActive = false;
+            string gameOverText = starsAchieved switch
+            {
+                0 => "Try Again!",
+                1 => "Good Job!",
+                2 => "Excellent!",
+                3 => "Perfect!!"
+            };
+            uiManager.gameOverText.text = gameOverText;
+            uiManager.newHighScoreText.SetActive(gridManager.Score > level.HighScore);
+            uiManager.gameOverMenu.SetActive(true);
+
+            level.UpdateHighScore(gridManager.Score);
+            level.UpdateStarsAchieved(gridManager.Score);
+        }
     }
     void LateUpdate()
     {
-        if(gridManager.MadeMove)
+        if (gridManager.MadeMove)
         {
             level.MovesRemaining--;
         }
