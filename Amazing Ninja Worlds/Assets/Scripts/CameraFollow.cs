0bf202698189023b45dfabe086db69fd16770e82
codeninjascf <cnchandlersford@gmail.com>
2023-03-28 16:21:58 +0100

    finished amazing ninjas worlds part 1 BUT FIX TEH GIANT BUGS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Amazing Ninja Worlds/Assets/Scripts/CameraFollow.cs
+++ Amazing Ninja Worlds/Assets/Scripts/CameraFollow.cs
@@ -6,16 +6,17 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float smoothingTime=.2f;
-    
+    public Vector3 cameraOffset;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
     // Update is called once per frame
+    Vector3 targetPos = new Vector3(TargetJoint2D.position.x, target.position.x, target.position.y, transform.position.z) + cameraOffset;
+    transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref _velocity, smoothingTime);
     void Update()
     {
-        
+
     }
 }
