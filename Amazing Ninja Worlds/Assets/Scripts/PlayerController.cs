3c276ec2c4640fdf8b17e67f52c1c8d14b45fdea
codeninjascf <cnchandlersford@gmail.com>
2023-05-23 16:48:48 +0100

    start from the last bit of part 5 when you add the fixedupdate update

--- Amazing Ninja Worlds/Assets/Scripts/PlayerController.cs
+++ Amazing Ninja Worlds/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour
     private bool _isGrounded;
     private Rigidbody2D _rigidbody;
     private bool _enabled;
+    private Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +24,9 @@ public class PlayerController : MonoBehaviour
     {
         if (!_enabled) return;
         float movement=moveSpeed*Input.GetAxisRaw("Horizontal");
+        _animator.SetBool("Moving", movement != 0);
         _rigidbody.position+=movement*Time.deltaTime*Vector2.right;
+
     }
     // Update is called once per frame
     void Update()
@@ -32,8 +35,14 @@ public class PlayerController : MonoBehaviour
         _isGrounded=Physics2D.Raycast(transform.position, Vector2.down, groundDistanceThreshold, whatIsGround);
         if(_isGrounded&&Input.GetButtonDown("Jump"))
         {
+            _animator.SetBool("Jumping", true);
             _rigidbody.velocity=Vector2.up*jumpForce;
         }
+        else
+        {
+            _animator.SetBool("Jumping", false);
+        }
+        _animator.SetBool("Falling", !_isGrounded);
     }
     public void Enable()
     {
@@ -61,5 +70,12 @@ public class PlayerController : MonoBehaviour
             gameManager.GotCollectible(other.transform);
             other.gameObject.SetActive(false);
         }
+    }
+    public void Disable()
+    {
+        _enabled = false;
+        _animator.SetBool("Moving", false);
+        _animator.SetBool("Jumping", false);
+        _animator.SetBool("Falling", !_isGrounded);
     }
 }
